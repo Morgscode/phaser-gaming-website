0d97a367fada53b3d1c788bc6ce48f8f4959721f
Luke Morgan <morgan.luke94@gmail.com>
2020-10-24 14:01:09 +0100

    adds renadom star generation feature to sandbox game

--- public/sandbox/src/sandboxApp.js
+++ public/sandbox/src/sandboxApp.js
@@ -23,12 +23,26 @@ const uiController = (() => {
     },
     renderScenePlatforms: (gameObject) => {
       const platforms = gameObject.physics.add.staticGroup();
-      platforms.create(400, 580, 'ground').setScale(2).refreshBody();
-      platforms.create(400, 450, 'ground');
-      platforms.create(100, 350, 'ground');
-      platforms.create(600, 250, 'ground');
-      platforms.create(75, 225, 'ground');
-      platforms.create(300, 100, 'ground');
+      // ground layer
+      platforms.create(400, 600, 'ground').setScale(2).refreshBody();
+      // 2nd left
+      platforms.create(250, 500, 'ground');
+      // 2nd right
+      platforms.create(750, 500, 'ground');
+      // 3rd left
+      platforms.create(-100, 400, 'ground');
+      // 3rd middle
+      platforms.create(400, 400, 'ground');
+      // 3rd right
+      platforms.create(900, 400, 'ground');
+      // 4th left
+      platforms.create(150, 300, 'ground');
+      // 4th right
+      platforms.create(650, 300, 'ground');
+      // 5th left
+      platforms.create(0, 200, 'ground');
+      // 5th right
+      platforms.create(500, 200, 'ground');
       return platforms;
     },
 
@@ -48,6 +62,14 @@ const uiController = (() => {
     bindCursorKeys: (gameObject) => {
       const cursors = gameObject.input.keyboard.createCursorKeys();
       return cursors;
+    },
+    renderScoreBoardText: (gameObject, score) => {
+      const scoreText = gameObject.add.text(16, 16, `score: ${score}`, {
+        fontSize: `32px`,
+        fontFamily: 'Courier',
+        fill: `#303030`,
+      });
+      return scoreText;
     },
   };
 
@@ -69,17 +91,27 @@ const uiController = (() => {
       return gameObject;
     },
     starCollected: (player, star) => {
-      return star.disableBody(true, true);
+      star.disableBody(true, true);
+      let score = parseInt(window.localStorage.getItem('playerOneScore'));
+      score += 10;
+      return window.localStorage.setItem('playerOneScore', score);
+    },
+    renderRandomStarGroup: (stars) => {
+      stars.children.iterate((child) => {
+        yVal = Phaser.Math.Between(0, 500);
+        child.enableBody(true, child.x, yVal, true, true);
+      });
+      return stars;
     },
   };
 
   const playerOneSetupFunctions = {
     renderPlayerOne: (gameObject) => {
-      const playerOne = gameObject.physics.add.sprite(50, 500, 'dude');
+      const playerOne = gameObject.physics.add.sprite(50, 545, 'dude');
       return playerOne;
     },
     setPlayerOneInitialPhysics: (player) => {
-      player.setBounce(0.5);
+      player.setBounce(0.2);
       player.body.setGravity(0, 300);
       player.setCollideWorldBounds(true);
       return player;
@@ -98,7 +130,7 @@ const uiController = (() => {
       gameObject.anims.create({
         key: 'turn',
         frames: [{ key: 'dude', frame: 4 }],
-        frameRate: 20,
+        frameRate: 10,
       });
 
       gameObject.anims.create({
@@ -142,7 +174,7 @@ const uiController = (() => {
        */
       // --- jump
       if (cursors.up.isDown && player.body.touching.down) {
-        player.setVelocityY(-330);
+        player.setVelocityY(-275);
         dataCtrl.setJumpCount();
       }
 
@@ -163,7 +195,7 @@ const uiController = (() => {
         player.body.velocity.y < 30 &&
         player.body.velocity.y > -100
       ) {
-        player.setVelocityY(-330);
+        player.setVelocityY(-275);
         dataCtrl.resetJumpCount();
       }
 
@@ -178,7 +210,7 @@ const uiController = (() => {
         player,
         stars,
         collectStarFn,
-        false,
+        null,
         gameObject
       );
       return gameObject;
@@ -201,6 +233,8 @@ const uiController = (() => {
       playerOneSetupFunctions.setupPlayerPlatformCollision,
     setPlayerStarCollisions: playerOneSetupFunctions.setupPlayerStarCollection,
     evaluatePlayerOneMovement: playerOneSetupFunctions.setupPlayerOneMovement,
+    renderScoreText: initialSceneSetupFunctions.renderScoreBoardText,
+    renderRandomStars: starSetupFunctions.renderRandomStarGroup,
   };
 })();
 
@@ -223,10 +257,21 @@ const dataController = (() => {
     },
   };
 
+  const scoreState = {
+    setGameScore: () => {
+      return window.localStorage.setItem('playerOneScore', 0);
+    },
+    getGameScore: () => {
+      return parseInt(window.localStorage.getItem('playerOneScore'));
+    },
+  };
+
   return {
     setJumpCount: playerJumpState.setJumpCount,
     resetJumpCount: playerJumpState.resetJumpCount,
     getJumpCount: playerJumpState.getJumpCount,
+    setupPlayerOneScore: scoreState.setGameScore,
+    getPlayerOneGameScore: scoreState.getGameScore,
   };
 })();
 
@@ -248,7 +293,7 @@ const gameController = ((uiCtrl, dataCtrl) => {
       default: 'arcade',
       arcade: {
         gravity: { y: 300 },
-        debug: true,
+        debug: false,
       },
     },
     scene: {
@@ -264,6 +309,8 @@ const gameController = ((uiCtrl, dataCtrl) => {
   let player;
   let platforms;
   let stars = [];
+  let scoreText;
+  let activeStarGroups;
 
   //----- GAME SCENE FUNCTIONS DEFINITIONS
   /**
@@ -280,31 +327,37 @@ const gameController = ((uiCtrl, dataCtrl) => {
   }
 
   function create() {
+    dataCtrl.setupPlayerOneScore();
     uiCtrl.renderSky(this);
     platforms = uiCtrl.renderPlatform(this);
-    stars[0] = uiCtrl.renderStars(this, 12, 300, 70);
-    stars[1] = uiCtrl.renderStars(this, 12, 0, 70);
-    stars.forEach((starGroup) => {
-      uiCtrl.setStarPlatformCollisions(this, starGroup, platforms);
-    });
+    stars.push(uiCtrl.renderStars(this, 5, 450, 70));
+    stars.push(uiCtrl.renderStars(this, 15, 350, 60));
+    stars.push(uiCtrl.renderStars(this, 25, 165, 50));
+    uiCtrl.setStarPlatformCollisions(this, stars, platforms);
     player = uiCtrl.renderPlayerOne(this);
     uiCtrl.setPlayerOnePhysics(player);
     uiCtrl.setPlayerPlatformCollisions(this, player, platforms);
     uiCtrl.setPlayerOneAnimations(this);
     cursors = uiCtrl.setupCursorKeys(this);
     uiCtrl.displayTitle(this);
+    scoreText = uiCtrl.renderScoreText(this, dataCtrl.getPlayerOneGameScore());
   }
 
   function update() {
     uiCtrl.evaluatePlayerOneMovement(player, cursors, dataCtrl);
+    uiCtrl.setPlayerStarCollisions(player, stars, uiCtrl.collectStar, this);
+    scoreText.setText(`Score: ${dataCtrl.getPlayerOneGameScore()}`);
+    activeStarGroups = 3;
     stars.forEach((starGroup) => {
-      uiCtrl.setPlayerStarCollisions(
-        player,
-        starGroup,
-        uiCtrl.collectStar,
-        this
-      );
+      if (starGroup.countActive() === 0) {
+        activeStarGroups--;
+      }
     });
+    if (activeStarGroups === 0) {
+      stars.forEach((starGroup) => {
+        uiCtrl.renderRandomStars(starGroup);
+      });
+    }
   }
 
   // LET'S MAKE THE GAME AVAILABLE TO THE GLOBAL SCOPE
